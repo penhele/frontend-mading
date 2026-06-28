2f67b2e1b85b7df1b6a10334b1d3fe1ac302f922
Stephen Helenus <stephenhelk@gmail.com>
2026-06-28 20:10:55 +0700

    feat: add sidebar

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,19 @@
+import { AppSidebar } from "@/components/layouts/app-sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+
+      <main className="flex-1">
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  );
+}
