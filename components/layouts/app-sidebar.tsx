8fa14b406f53e78f671296dfd66f788902e60585
Stephen Helenus <stephenhelk@gmail.com>
2026-06-28 20:44:20 +0700

    feat: add dashbord page

--- components/layouts/app-sidebar.tsx
+++ components/layouts/app-sidebar.tsx
@@ -4,15 +4,38 @@ import {
   SidebarFooter,
   SidebarGroup,
   SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { ROUTES } from "@/contants/routes";
+import { LayoutDashboard } from "lucide-react";
 
 export function AppSidebar() {
+  const navItems = [
+    {
+      label: "Dashboard",
+      Icon: LayoutDashboard,
+      route: ROUTES.DASHBOARD,
+    },
+  ];
+
   return (
-    <Sidebar>
+    <Sidebar variant="floating">
       <SidebarHeader />
       <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
+        <SidebarGroup>
+          <SidebarMenu>
+            {navItems.map((item, index) => (
+              <SidebarMenuItem key={index}>
+                <SidebarMenuButton>
+                  <item.Icon />
+                  {item.label}
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
       </SidebarContent>
       <SidebarFooter />
     </Sidebar>
